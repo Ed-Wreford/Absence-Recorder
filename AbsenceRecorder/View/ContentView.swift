54f7d44ca0b487a12b0c2417760a19f875e6ba66
Ed-Wreford <wreford.e@etoncollege.org.uk>
2023-01-30 15:30:31 +0000

    Part 2 done, still

--- AbsenceRecorder/View/ContentView.swift
+++ AbsenceRecorder/View/ContentView.swift
@@ -9,12 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var divisions: [Division]
+    @State private var currentDate: Date = Date()
     
     var body: some View {
-
-        Text("Code: \(divisions[0].code)")
-            .padding()
-        
+        NavigationView {
+            List(divisions, id: \.self.code) { division in
+                DivisionItem(division: division)
+                
+            }
+            .navigationTitle(currentDate.getShortDate())
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button(action: { currentDate = currentDate.previousDay() }) {
+                        Image(systemName: "arrow.backward")
+                    }
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: { currentDate = currentDate.nextDay() }) {
+                        Image(systemName: "arrow.forward")
+                    }
+                }
+            }
+        }
     }
 }
 
