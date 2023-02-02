71fd7c0b4f53ded8a9d7e42ab0528c80e7dd1ac9
Ed-Wreford <wreford.e@etoncollege.org.uk>
2023-02-02 10:34:07 +0000

    Do task 1

---
+++ AbsenceRecorder/View/StatisticsView.swift
@@ -0,0 +1,20 @@
+//
+//  StatisticsView.swift
+//  AbsenceRecorder
+//
+//  Created by Ed Wreford on 02/02/2023.
+//
+
+import SwiftUI
+
+struct StatisticsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StatisticsView_Previews: PreviewProvider {
+    static var previews: some View {
+        StatisticsView()
+    }
+}
