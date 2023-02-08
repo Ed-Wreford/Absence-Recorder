315a91b3d0d1a84ced0428f9b126976ead01c4af
Ed-Wreford <wreford.e@etoncollege.org.uk>
2023-02-08 21:20:37 +0000

    part 4 done

--- AbsenceRecorder/Model/Division.swift
+++ AbsenceRecorder/Model/Division.swift
@@ -16,13 +16,33 @@ class Division {
         self.code = code
     }
     
-    #if DEBUG
+    func getAbsence(for date: Date) -> Absence? {
+        
+        return absences.first {
+            let comparison = Calendar.current.compare($0.takenOn, to: date, toGranularity: .day)
+            return comparison == .orderedSame
+        }
+        
+    }
     
+    func createAbsenceOrGetExistingIfAvailable(for date: Date) -> Absence {
+        
+        if let existingAbsence = getAbsence(for: date) {
+            return existingAbsence
+        } else {
+            let absence = Absence(date: date, students: students)
+            absences.append(absence)
+            
+            return absence
+        }
+        
+    }
+    
+    #if DEBUG
     static func createDivision(code: String, of size: Int) -> Division {
         let division = Division(code: code)
         
         for i in 1...size {
-            
             let student = Student(forename: "Forename\(i)", surname: "Surname\(i)", birthday: Date())
             division.students.append(student)
             
