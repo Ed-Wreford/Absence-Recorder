b53dd47344046ab979038e4689ef2bbbf70b2bd6
Ed-Wreford <wreford.e@etoncollege.org.uk>
2023-02-08 20:59:36 +0000

    Video 12 complete

---
+++ AbsenceRecorder/Model/Absence.swift
@@ -0,0 +1,25 @@
+//
+//  Absence.swift
+//  AbsenceRecorder
+//
+//  Created by Ed Wreford on 08/02/2023.
+//
+
+import Foundation
+
+class Absence {
+    
+    let takenOn: Date
+    var studentAbsences: [StudentAbsence]
+    
+    init(date: Date, students: [Student]) {
+        
+        takenOn = date
+        studentAbsences = students.map { StudentAbsence(student: $0) }
+        
+    }
+    
+    #if DEBUG
+    static let example = Absence(date: Date(), students: Student.examples)
+    #endif
+}
